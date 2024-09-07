c34133cfe1943f7def39d17f899f5b1a5fb35934
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-07 18:38:29 +0300

    add classed bassed django create , edit , detials

--- post/views.py
+++ post/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 
 from .models import Post
 from .forms import PostForm
+from django.views.generic import ListView , DetailView
+from django.views.generic.edit import CreateView , DeleteView , UpdateView
+
 
 def post_list(request):
     all_posts = Post.objects.all()
@@ -27,6 +30,7 @@ def post_create(request):
 
 
 def post_edit(request , id):
+
     post = Post.objects.get(id=id)
     if request.method == "POST":
         print('in post')
@@ -37,3 +41,24 @@ def post_edit(request , id):
         print('in else')
         form = PostForm(instance=post)
     return render(request , 'post/post_edit.html',{'form':form})
+
+
+class PostList(ListView):
+    model = Post
+
+class PostDetails(DetailView):
+    model = Post
+    template_name = 'post/post_details.html'
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title','content','created_at']
+    template_name = 'post/post_edit.html'
+    success_url = '/blog/cbv'
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title','content','created_at' , 'published','email','views_count','category']
+    template_name = 'post/post_create.html'
+    success_url = '/blog/cbv'
+
