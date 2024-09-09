44cd1399d012af7af442d0c31d1b3c7bc5c2d94f
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-10 02:21:08 +0300

    create profile when signup user

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model):
@@ -7,3 +9,13 @@ class Profile(models.Model):
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
     image = models.ImageField(upload_to = 'users/')
+
+    def __str__(self):
+        return self.user
+    
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
