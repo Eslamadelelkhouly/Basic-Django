ac74eb3ba018558eda63c922390eefb44356e157
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-10 00:37:09 +0300

    create models account

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
+    image = models.ImageField(upload_to = 'users/')
