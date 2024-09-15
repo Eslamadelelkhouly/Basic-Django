b0b809c5056c8de427418085e94171cf54ecc687
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-15 16:23:00 +0300

    add url auth

--- blog/urls.py
+++ blog/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('post.urls' , namespace='blog')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
