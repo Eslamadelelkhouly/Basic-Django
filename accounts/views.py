9877bc520c51cc8db9f1c3f06263faa31209a5f7
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-15 19:11:28 +0300

    add signup method

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-
+from .forms import SignupForm
+from .models import Profile
+from django.contrib.auth.models import User
 # Create your views here.
 
 def signup(request):
-    pass
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = SignupForm()
+    return render(request,"registration/signup.html",{'form':form})
 
 def profile(request):
     pass
