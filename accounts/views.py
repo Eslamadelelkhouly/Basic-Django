724902e073fdbe2d8039ce0d6da71989f4e9b436
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-15 18:45:04 +0300

    add signup urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def signup(request):
+    pass
+
+def profile(request):
+    pass
+
+
+def edit_profile(request):
+    pass
--- blog/urls.py
+++ blog/urls.py
@@ -21,9 +21,10 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('accounts/', include('accounts.urls' , namespace='accounts')),
     path('admin/', admin.site.urls),
     path('blog/', include('post.urls' , namespace='blog')),
-    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
